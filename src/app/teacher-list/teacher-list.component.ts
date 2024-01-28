439d428d672f0d4216002de5fa0d8943aec01007
Sam Tyagi <samtyagi0001@gmail.com>
2024-01-28 22:45:14 +0530

    sweetalert add in

--- src/app/teacher-list/teacher-list.component.ts
+++ src/app/teacher-list/teacher-list.component.ts
@@ -8,30 +8,30 @@ import { Component } from '@angular/core';
 export class TeacherListComponent {
 
 
-  
+
   data = [
-    { id: 1, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 2, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 3, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 4, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 5, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 6, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 7, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 8, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 9, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 10, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 11, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 12, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 13, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 14, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 15, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 16, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 17, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 18, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 19, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 20, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 21, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
-    { id: 22, name: 'sam', age: 27, email:'samtyagi@gmail.com' , phone:9761431498 },
+    { id: 1, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 2, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 3, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 4, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 5, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 6, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 7, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 8, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 9, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 10, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 11, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 12, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 13, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 14, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 15, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 16, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 17, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 18, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 19, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 20, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 21, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
+    { id: 22, name: 'sam', age: 27, email: 'samtyagi@gmail.com', phone: 9761431498 },
 
   ];
 }
